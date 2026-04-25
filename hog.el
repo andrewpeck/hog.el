017a916e7923eb07655685ae977d8b5965030f67
Andrew Peck <peckandrew@gmail.com>
2026-04-25 11:12:25 -0700

    fix: properly error if project-root is not found

--- hog.el
+++ hog.el
@@ -84,12 +84,11 @@ Can be set in dir-locals to be changed on a per-project basis.")
 
 (defun hog--project-root ()
   "Get the root of the current version controlled project."
-  (let ((root
-         (expand-file-name (or (and (fboundp 'projectile-project-root) (projectile-project-root))
-                               (vc-root-dir)
-                               (locate-dominating-file default-directory ".git")))))
+  (let ((root (or (and (fboundp 'projectile-project-root) (projectile-project-root))
+                  (vc-root-dir)
+                  (locate-dominating-file default-directory ".git"))))
     (unless root (user-error "No root directory found!"))
-    root))
+    (expand-file-name root)))
 
 (defun hog--get-projects ()
   "Get a list of available Hog projects."
